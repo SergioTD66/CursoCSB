9ebb3f7e1e04c2fe47fbae909841e7c6ce078aa8
SergioTD <sergiotadeu66@gmail.com>
2021-09-04 13:18:41 -0300

    Primeiro desafio do curso

--- CursoAlelogit/src/CursoParte2/Exercicio6.java
+++ CursoAlelogit/src/CursoParte2/Exercicio6.java
@@ -1,29 +1,16 @@
 package CursoParte2;
 
 import java.util.Scanner;
-
-public class Exercicio6 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 /*
     Faça a implementação do Jogo Pedra, Papel e Tesoura (Jokempô).
     O algoritmo deverá perguntar qual é a escolha do jogador 1
     (Pedra [pe], Papel [pa], Tesoura [t]) e deverá fazer o mesmo para o jogador 2.
     No final da execução o algoritmo deverá dizer qual é o jogador vencedor ou se houve empate.
     * */
+public class Exercicio6 {
+    public static void jokenpo() {
+        Scanner scanner = new Scanner(System.in);
 
-//        System.out.println(" Jogador 1 - Digite 1 para PEDRA, 2 para PAPEL ou 3 para TESOURA: ");
-//        int a = scanner.nextInt();
-//
-//        System.out.println("Jogador 2 - Digite 1 para PEDRA, 2 para PAPEL ou 3 para TESOURA: ");
-//        int b = scanner.nextInt();
-//
-//        if (a == b)
-//            System.out.println("Empate");
-//        else if (a == 1 && b == 2 || a == 2 && b == 3 || a == 3 && b == 1)
-//            System.out.println("Jogador 2 venceu!");
-//        else
-//            System.out.println("Jogador 1 venceu!");
 
             System.out.println(" Jogador 1 - Digite 0 para PEDRA, 1 para PAPEL ou 2 para TESOURA: ");
             int jogador1 = scanner.nextInt();
@@ -38,6 +25,22 @@ public class Exercicio6 {
             else
                 System.out.println("Jogador 2 venceu!");
 
+            scanner.close();
+
+
+
+//        System.out.println(" Jogador 1 - Digite 1 para PEDRA, 2 para PAPEL ou 3 para TESOURA: ");
+//        int a = scanner.nextInt();
+//
+//        System.out.println("Jogador 2 - Digite 1 para PEDRA, 2 para PAPEL ou 3 para TESOURA: ");
+//        int b = scanner.nextInt();
+//
+//        if (a == b)
+//            System.out.println("Empate");
+//        else if (a == 1 && b == 2 || a == 2 && b == 3 || a == 3 && b == 1)
+//            System.out.println("Jogador 2 venceu!");
+//        else
+//            System.out.println("Jogador 1 venceu!");
 
             scanner.close();
 
@@ -82,5 +85,9 @@ public class Exercicio6 {
                 System.out.println("não existe essa opção");
                 break;
         }*/
+public static void main(String[] args) {
+
+}
     }
+
 
--- CursoAlelogit/src/Extra/Extra1.java
+++ CursoAlelogit/src/Extra/Extra1.java
@@ -42,9 +42,102 @@ public class Extra1 {
             System.out.println(antecessor + i);
             antecessor++;
         }
+
     }
+    public static void bananaRev() {
+        // para i = 0
+        // até i < 10
+        // i = i + 1
+
+        String input = "Banana";
+
+        for (int i = 0; i <= input.length(); i++) {
+            for (int j = 0; j < input.length(); j++) {
+                int posicao;
+                if (j + i > input.length() - 1) {
+                    posicao = input.length() - i - j;
+
+                    if (posicao < 0) {
+                        posicao *= -1;
+                    }
+
+                } else {
+                    posicao = j + i;
+                }
+                System.out.print(input.charAt(posicao));
+            }
+            System.out.println();
+        }
+    }
+    public static void bananaTeste() {
+        // para i = 0
+        // até i < 10
+        // i = i + 1
+
+        String input = "Banana";
+
+        for (int i = 0; i <= input.length(); i++) {
+            for (int j = 0; j < input.length(); j++) {
+                int posicao;
+                if (j + i > input.length() - 6) {
+                    posicao = input.length() - i - j;
+
+                    if (posicao < 0) {
+                        posicao *= -1;
+                    }
+
+                } else {
+                    posicao = j + i;
+                }
+                System.out.print(input.charAt(posicao));
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void bananaNovo() {
+        // para i = 0
+        // até i < 10
+        // i = i + 1
+
+        String input = "Banana";
+
+        for (int i = 0; i <= input.length(); i++) {
+            for (int j = 0; j < input.length(); j++) {
+                int posicao;
+                if (j + i > input.length() - 1) {
+                    posicao = input.length() - i - j;
+
+                    if (posicao < 0) {
+                        posicao *= -1;
+                    }
+
+                } else {
+                    posicao = j + i;
+                }
+                System.out.print(input.charAt(posicao));
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void bananaSubStr() {
+        String input = "Banana";
+        for (int i = 0; i <= input.length(); i++) {
+            System.out.println(input.substring(i) + input.substring(0, i));
+        }
+
+        System.out.println("--- By Sergio");
+
+        for (int i = 0; i <= input.length(); i++) {
+            System.out.println(input.substring(input.length() - i) + input.substring(0, input.length() - i));
+        }
+    }
+
     public static void main(String[] args) {
-        finonnaci();
+        bananaTeste();
     }
 }
 
